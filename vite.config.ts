e4d1ffc8b3df8afa340a1d39e4893baad4b393ac
Roman Pretty <rjjpretty@gmail.com>
2025-02-25 20:06:28 +0000

    Deployed

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,20 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from '@tailwindcss/vite'
 
 import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-    base:
-        mode == "development"
-            ? "http://localhost:5173/"
-            : "/static/api/spa/",
-    build: {
-        emptyOutDir: true,
-        outDir: "../api/static/api/spa",
-    },
-    plugins: [vue(),tailwindcss(),
-],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-        },
-    },
-}));
+export default defineConfig(({mode}) => ({
+    base: "/swrpg-datapad/",
+    plugins: [vue(), tailwindcss()],
+resolve: {
+    alias: {
+        '@'
+    :
+        path.resolve(__dirname, './src'),
+    }
+,
+}
+,
+}))
+;
