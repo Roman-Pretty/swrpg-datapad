6dce5b144767704aeb3b01a48a54b8615dfe3e31
Roman Pretty <rjjpretty@gmail.com>
2025-02-25 20:04:08 +0000

    Updates

--- src/pages/Maneuvers.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="h-full flex items-center justify-center">
-    <div class="loading absolute left-1/2 top-1/2 -z-10"/>
-    <img src="../assets/Combat%20-%20Maneuvers.png" class="h-full object-contain" alt="Combat Actions">
-  </div>
-</template>
