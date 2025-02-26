a547553b881259045750ac6ab8a08f2b3028d91d
Roman Pretty <rjjpretty@gmail.com>
2025-02-26 13:07:59 +0000

    Removed loading indicator as it was bugging on mobile

--- src/pages/Environment.vue
+++ src/pages/Environment.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="h-full flex items-center justify-center">
-    <div class="loading absolute left-1/2 top-1/2 -z-10"/>
     <img src="../assets/Environmental%20Effects.png" class="h-full object-contain" alt="Combat Actions">
   </div>
 </template>
--- src/pages/ItemQualitiesPassive.vue
+++ src/pages/ItemQualitiesPassive.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="h-full flex items-center justify-center">
-    <div class="loading absolute left-1/2 top-1/2 -z-10"/>
     <img src="../assets/Item%20Qualities%20-%20Passive.png" class="h-full object-contain" alt="Combat Actions">
   </div>
 </template>
