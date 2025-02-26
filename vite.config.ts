0dd33af446ef1f0cb07edaf5c72f9ca9ea75bff5
Roman Pretty <rjjpretty@gmail.com>
2025-02-26 16:04:00 +0000

    Linked domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
 
 export default defineConfig(({mode}) => ({
-    base: "/swrpg-datapad/",
+    base: "/",
     plugins: [vue(), tailwindcss()],
 resolve: {
     alias: {
